0ddade839f0627100632d4b2211a780f9fa5fb38
wukunyun <864353360@qq.com>
2022-05-15 20:15:24 +0800

    fix maven download error

---
+++ B-API/src/main/java/com/b/Application.java
@@ -0,0 +1,9 @@
+package com.b;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+    }
+}
